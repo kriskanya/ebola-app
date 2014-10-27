4961a8aff73edf17c9f0747d6298d1c7903583d0
Kristian Kanya <kriskanya@gmail.com>
2014-10-26 22:05:10 -0500

    a few tests written

--- e2e/main/main.spec.js
+++ e2e/main/main.spec.js
@@ -5,19 +5,45 @@ describe('Main View', function() {
 
   beforeEach(function() {
     browser.get('/');
-    page = require('./main.po');
+    // page = require('./main.po');
   });
 
-  it('should include jumbotron with correct data', function() {
-    element(by.model('name')).sendKeys("Bob Smith");
-    element(by.model('city')).sendKeys("San Antonio");
+  it('should create a new record and display it on the page', function() {
+    element(by.model('name')).sendKeys('Bob Smith');
+    element(by.model('city')).sendKeys('San Antonio');
+    element(by.model('infectionDate')).sendKeys('10-24-2014');
 
     element(by.id('newPatientSubmitButton')).click();
 
     expect(element(by.binding('p.name')).getText()).
-      toEqual('Name: Bob Smith');
+      toEqual('Bob Smith');
+    expect(element(by.binding('p.city')).getText()).
+      toEqual('San Antonio');
+    expect(element(by.binding('p.infectionDate')).getText()).
+      toEqual('Oct 24, 2014');
     // expect(page.h1El.getText()).toBe('Ebola Patient App');
     // expect(page.imgEl.getAttribute('src')).toMatch(/assets\/images\/yeoman.png$/);
     // expect(page.imgEl.getAttribute('alt')).toBe('I\'m Yeoman');
+  });
+
+  it('should search and find the record that was just created', function() {
+    element(by.model('query')).sendKeys('Bob Smith');
+
+    expect(element(by.css('.allPatients')).getText()).
+      toEqual('Bob Smith San Antonio Oct 24, 2014 X');
+  });
+
+  it('should sort alphabetically by city', function() {
+    element(by.model('name')).sendKeys('Adam Apple');
+    element(by.model('city')).sendKeys('Austin');
+    element(by.model('infectionDate')).sendKeys('10-26-2014');
+
+    element(by.id('newPatientSubmitButton')).click();
+
+    var ptor = protractor.getInstance();
+    ptor.findElement(protractor.By.css('select option:nth-child(2)')).click();
+
+    expect(element(by.css('.allPatients')).getText()).
+      toEqual('Adam Apple Austin Oct 26, 2014 X');
   });
 });
